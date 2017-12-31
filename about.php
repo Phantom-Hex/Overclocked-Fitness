2bfec902eb9d588e3e5a89817946c4d872c3141f
Tony T <tonytatsumaki@live.com>
2017-12-31 09:56:59 -0500

    Modified the about page.

--- about.php
+++ about.php
@@ -2,10 +2,13 @@
 <?php require("inc/heading.php"); ?>
 <section class="container-fluid">
 	<h2 class="display-4">About Overclocked Fitness</h2>
-    <p class="text-justify">Overclocked Fitness was originally started as a self-proprietorship by head coach (and lean, mean, fighting machine) Tony Tatsumaki. In bringing the fundamentals of calisthenic workouts into the gym with complex resistance routines, Tony has pioneered an efficient way for beginners and advanced strength training without the need of a gym membership or heavy weight.</p>
-    <img class="img-thumbnail rounded float-md-left" src="./img/runner.jpeg" width="400px">
-    <p class="text-justify">The addition of the tiers came at the behest of his personal circle of friends when they wanted to get into fitness. Computers and all-around geekery were all anyone knew in the group, so then the idea of the Hero Complex came to mind.  Each tier was initially designed around the familiar faces in graphic novels and japanese animation (the fancier terms for comics and anime) such as Goku, Batman, Joe the Condor, Harley Quinn, and Lightning Farron.</p>
-    <img class="img-thumbnail rounded float-md-right" src="./img/shoes.jpeg" width="400px">
+    <div class="text-center mx-auto">
+        <p class="text-justify">Overclocked Fitness was originally started as a self-proprietorship by head coach (and lean, mean, fighting machine) Tony Tatsumaki. In bringing the fundamentals of calisthenic workouts into the gym with complex resistance routines, Tony has pioneered an efficient way for beginners and advanced strength training without the need of a gym membership or heavy weight.</p>
+        <img class="img-thumbnail rounded float-xl-left" src="./img/runner.jpeg" width="600px">
+        <p class="text-justify">The addition of the tiers came at the behest of his personal circle of friends when they wanted to get into fitness. Computers and all-around geekery were all anyone knew in the group, so then the idea of the Hero Complex came to mind.  Each tier was initially designed around the familiar faces in graphic novels and japanese animation (the fancier terms for comics and anime) such as Goku, Batman, Joe the Condor, Harley Quinn, and Lightning Farron.</p>
+        <img class="img-thumbnail rounded float-xl-right" src="./img/shoes.jpeg" width="600px">
+    </div>
     <p class="text-justify">When the concept of an online exercise group became viable, the need for a hook had to happen with it. The additional hacking element was added to support the need for coders in the IT world; and that became another option for the uninitiated. Every client with Overclocked has to create a web page for themselves using raw html/css code they submit to an associated GitHub account; and at the end of their term, they're responsible for making their own before and after sites. With a new outlook on life, it's nice to be in control, right?</p>
 </section>
+<br /><br /><br />
 <?php require("inc/footing.php"); ?>
